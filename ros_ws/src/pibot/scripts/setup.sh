01bf0ab68ee4d1885f3fc317011e59089c7215ed
surewin1102 <surewin1102@163.com>
2018-03-01 16:10:43 +0800

    add ros ws

---
+++ ros_ws/src/pibot/scripts/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "setup pibot modules"
+echo " "
+sudo cp `rospack find pibot`/rules/pibot.rules  /etc/udev/rules.d
+sudo cp `rospack find pibot`/rules/rplidar.rules  /etc/udev/rules.d
+echo " "
+echo "Restarting udev"
+echo ""
+sudo service udev reload
+sudo service udev restart
+echo "finish "
