ce7851f707ce9c36b190d262fc849a4cd146e78e
surewin1102 <surewin1102@163.com>
2018-03-05 18:12:02 +0800

    add eai x4 support

--- ros_ws/src/pibot/scripts/setup.sh
+++ ros_ws/src/pibot/scripts/setup.sh
@@ -4,6 +4,7 @@ echo "setup pibot modules"
 echo " "
 sudo cp `rospack find pibot`/rules/pibot.rules  /etc/udev/rules.d
 sudo cp `rospack find pibot`/rules/rplidar.rules  /etc/udev/rules.d
+sudo cp `rospack find pibot`/rules/ydlidar.rules  /etc/udev/rules.d
 echo " "
 echo "Restarting udev"
 echo ""
